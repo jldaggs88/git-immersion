9cce2a6bba627512f67cb3ec2b93018eebb27dc1
Jessa Daggs <jessalyn.daggs@gmail.com>
2020-05-18 22:08:19 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,13 @@
+const { Greeter } = require('./greeter');
+
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`What's your name`, (myName) => {
+  const greeter = new Greeter(name);
+  console.log(greeter.greet());
+
+  rl.close();
+});
